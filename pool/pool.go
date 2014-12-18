7fcf127503309d8870f4d59732751071afc5caec
Peter Bourgon <peter@bourgon.org>
2014-12-19 00:05:14 +0100

    Pool explained

--- pool/pool.go
+++ pool/pool.go
@@ -1,7 +1,15 @@
 package pool
 
-// Pool describes anything which can yield hosts.
+import "errors"
+
+var (
+	// ErrNoHosts indicates a pool is empty.
+	ErrNoHosts = errors.New("no hosts available")
+)
+
+// Pool describes anything which can yield hosts, and report on their
+// transactional success.
 type Pool interface {
-	Get() (string, error)
-	Put(string, bool)
+	Get() (host string, err error)
+	Put(host string, success bool)
 }
