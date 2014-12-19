83c642581d20cca4c3a4653eb44b81aac7ccebd7
Peter Bourgon <peter@bourgon.org>
2014-12-19 10:02:05 +0100

    Add prometheus to pool instrumentation

--- pool/instrumented.go
+++ pool/instrumented.go
@@ -1,12 +1,45 @@
 package pool
 
-import "expvar"
+import (
+	"expvar"
+	"fmt"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 var (
 	getCount       = expvar.NewInt("srvproxy_pool_get_count")
 	putOKCount     = expvar.NewInt("srvproxy_pool_put_ok_count")
 	putFailedCount = expvar.NewInt("srvproxy_pool_put_failed_count")
 	outstanding    = expvar.NewInt("srvproxy_pool_outstanding")
+)
+
+var (
+	promGetCount = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: "srvproxy",
+			Subsystem: "pool",
+			Name:      "get_count",
+			Help:      "Number of get requests.",
+		},
+	)
+	promPutCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "srvproxy",
+			Subsystem: "pool",
+			Name:      "put_count",
+			Help:      "Number of put requests.",
+		},
+		[]string{"success"},
+	)
+	promOutstanding = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Namespace: "srvproxy",
+			Subsystem: "pool",
+			Name:      "outstanding",
+			Help:      "Number of outstanding hosts.",
+		},
+	)
 )
 
 // Instrumented records metrics for operations against the wrapped Pool.
@@ -19,17 +52,21 @@ type instrumented struct{ Pool }
 func (i instrumented) Get() (string, error) {
 	getCount.Add(1)
 	outstanding.Add(1)
+	promGetCount.Add(1)
+	promOutstanding.Add(1)
 
 	return i.Pool.Get()
 }
 
 func (i instrumented) Put(s string, b bool) {
+	i.Pool.Put(s, b)
+
 	outstanding.Add(-1)
 	if b {
 		putOKCount.Add(1)
 	} else {
 		putFailedCount.Add(1)
 	}
-
-	i.Pool.Put(s, b)
+	promOutstanding.Sub(1)
+	promPutCount.WithLabelValues(fmt.Sprintf("%v", b)).Add(1)
 }
