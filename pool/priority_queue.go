b655b34e51224fcabf72d6c9329bb610e2f1f1cd
Peter Bourgon <peter@bourgon.org>
2014-12-21 15:18:21 +0100

    First cut at a priority-queue-like thing

--- pool/priority_queue.go
+++ pool/priority_queue.go
@@ -1,27 +1,75 @@
 package pool
 
-import (
-	"errors"
-	"sync"
-)
+import "sync/atomic"
 
 // PriorityQueue returns a Pool that yields hosts according to their recent
 // successes or failures.
 func PriorityQueue(hosts []string) Pool {
 	return &priorityQueue{
-		hosts: hosts,
+		hosts:  hosts,
+		scores: make([]int64, len(hosts)),
+		robin:  -1,
 	}
 }
 
 type priorityQueue struct {
-	sync.RWMutex
-	hosts []string
+	hosts  []string
+	scores []int64
+	robin  int64
 }
 
 func (pq *priorityQueue) Get() (string, error) {
-	return "", errors.New("not implemented")
+	if len(pq.hosts) <= 0 {
+		return "", ErrNoHosts
+	}
+
+	var robin int
+	for {
+		var (
+			vold = atomic.LoadInt64(&pq.robin)
+			vnew = vold + 1
+		)
+		if atomic.CompareAndSwapInt64(&pq.robin, vold, vnew) {
+			robin = int(vnew)
+			break
+		}
+	}
+
+	for { // find-a-zero
+		for i := range pq.scores { // check all the scores
+			index := (i + robin) % len(pq.scores)
+			for { // individual score CAS
+				if v := atomic.LoadInt64(&pq.scores[index]); v == 0 {
+					return pq.hosts[index], nil
+				} else if v < 0 {
+					panic("impossible")
+				} else if atomic.CompareAndSwapInt64(&pq.scores[index], v, v-1) {
+					break
+				}
+			}
+		}
+	}
 }
 
-func (pq *priorityQueue) Put(string, bool) {
-	// not implemented
+func (pq *priorityQueue) Put(host string, success bool) {
+	for i, candidate := range pq.hosts {
+		if candidate != host {
+			continue
+		}
+
+		for {
+			vold := atomic.LoadInt64(&pq.scores[i])
+
+			vnew := vold + 1
+			if success {
+				vnew = 0
+			}
+
+			if atomic.CompareAndSwapInt64(&pq.scores[i], vold, vnew) {
+				return
+			}
+		}
+	}
+
+	panic("bad Put")
 }
--- pool/priority_queue_test.go
+++ pool/priority_queue_test.go
@@ -1,7 +1,55 @@
 package pool_test
 
-import "testing"
+import (
+	"github.com/peterbourgon/srvproxy/pool"
 
-func TestPriorityQueue(t *testing.T) {
-	t.Skip()
+	"testing"
+)
+
+func TestPriorityQueueNoFailure(t *testing.T) {
+	var (
+		h = []string{"a", "b", "c"}
+		p = pool.PriorityQueue(h)
+	)
+
+	for i := 0; i < 10*len(h); i++ {
+		have, err := p.Get()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if want := h[i%len(h)]; want != have {
+			t.Errorf("i=%d: want %q, have %q", i, want, have)
+			continue
+		}
+
+		p.Put(have, true)
+	}
+}
+
+func TestPriorityQueueDistribution(t *testing.T) {
+	var (
+		h = []string{"a", "b", "c", "d", "e", "f"}
+		f = map[string]bool{"a": true, "c": true} // fail
+		p = pool.PriorityQueue(h)
+		m = map[string]int{}
+	)
+
+	n := 10 * len(h)
+	for i := 0; i < n; i++ {
+		host, err := p.Get()
+		if err != nil {
+			t.Errorf("i=%d: %s", i, err)
+			continue
+		}
+
+		m[host]++
+		success := !f[host]
+		//t.Logf("i=%d: %q (success %v)", i, host, success)
+		p.Put(host, success)
+	}
+
+	for _, host := range h {
+		t.Logf("%q: %.0f%%", host, (float64(m[host])/float64(n))*100.0)
+	}
 }
