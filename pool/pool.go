8f02b700531a26a2bea1c1af66f2411759ada8eb
Peter Bourgon <peter@bourgon.org>
2015-01-19 16:55:56 +0100

    Remove dead code

--- pool/pool.go
+++ pool/pool.go
@@ -1,6 +1,9 @@
 package pool
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+)
 
 var (
 	// ErrNoHosts indicates a pool is empty.
@@ -19,3 +22,11 @@ type Pool interface {
 
 // Factory converts a slice of hosts to a Pool.
 type Factory func([]string) Pool
+
+// SuccessFunc determines if an HTTP transaction is considered successful.
+// The result is noted by the pool.
+type SuccessFunc func(*http.Response, error) bool
+
+// SimpleSuccess is a SuccessFunc which returns true if there was no error.
+// The response status code is ignored.
+func SimpleSuccess(_ *http.Response, err error) bool { return err == nil }
